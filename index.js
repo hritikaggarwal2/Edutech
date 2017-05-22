8e88896123b6880b9ad4ddfb8a21ecbf4ab50f40
hritikaggarwal <hritikaggarwal2@gmail.com>
2017-05-22 19:10:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	// $(".all_bars").click(function(){
+	// 	document.styleSheets[0].addRule('.red::before','color: green');
+	// 	document.styleSheets[0].insertRule('.red::before { color: green }', 0);
+	// });
+});
