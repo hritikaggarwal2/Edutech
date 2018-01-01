f343a956a2c964d171781b68772dca7a911116fe
Raghav Mathur <raghav3501@gmail.com>
2018-01-01 18:16:14 +0530

    vids

--- courses.js
+++ courses.js
@@ -1,16 +1,79 @@
-var cpp = "0";
-var dood = "1";
-var abcdef = "2";
+var inte = "0";
+var engl = "1";
+var net = "2";
+var office = "5";
+var web = "6";
+var cpp = "7";
+var ps = "8";
+var dood = "9";
+var ext = "10";
+
+var abc = document.URL
+var ret = abc.replace('courses.html','vids.html')
+
+$(".inte").click(function () {
+	localStorage.setItem("storageName", inte);
+	console.log(inte);	
+	console.log(localStorage.getItem("storageName"));
+	window.open(ret, "_self")	
+})
+
+$(".engl").click(function () {
+	localStorage.setItem("storageName", engl);
+	console.log(engl);	
+	console.log(localStorage.getItem("storageName"));
+	window.open(ret, "_self")	
+})
+
+$(".net").click(function () {
+	localStorage.setItem("storageName", net);
+	console.log(net);	
+	console.log(localStorage.getItem("storageName"));
+	window.open(ret, "_self")	
+})
+
+$(".office").click(function () {
+	localStorage.setItem("storageName", office);
+	console.log(office);	
+	console.log(localStorage.getItem("storageName"));
+	window.open(ret, "_self")	
+})
+
+
+$(".web").click(function () {
+	localStorage.setItem("storageName", web);
+	console.log(web);	
+	console.log(localStorage.getItem("storageName"));
+	window.open(ret, "_self")	
+})
+
 $(".cpp").click(function () {
 	localStorage.setItem("storageName", cpp);
 	console.log(cpp);	
 	console.log(localStorage.getItem("storageName"));
-	window.open("https://hritikaggarwal.github.io/Edutech/vids.html", "_self")	
+	window.open(ret, "_self")	
 })
 
-$(".doodle").click(function () {
+$(".ps").click(function () {
+	localStorage.setItem("storageName", ps);
+	console.log(ps);	
+	console.log(localStorage.getItem("storageName"));
+	window.open(ret, "_self")	
+})
+
+$(".dood").click(function () {
 	localStorage.setItem("storageName", dood);
 	console.log(dood);	
 	console.log(localStorage.getItem("storageName"));
-	window.open("https://hritikaggarwal.github.io/Edutech/vids.html", "_self")	
+	window.open(ret, "_self")	
 })
+
+$(".ext").click(function () {
+	localStorage.setItem("storageName", ext);
+	console.log(ext);	
+	console.log(localStorage.getItem("storageName"));
+	window.open(ret, "_self")	
+})
+
+
+
--- vids.js
+++ vids.js
@@ -1,60 +1,22 @@
-/*var f = new Array()
+var f = new Array()
+f[0] = ["cQfIGvfeqYw", "BNQbmshpskM"]
+f[1] = ["fj5Fa85IAxY", "AUAsTV7pD28", "DLB06ug08tY", "HAMjqSYkhM", "DvTIHYBJSBg-TY", "jnTDxDUJT78"]
+f[2] = ["920julHeFw"]
+f[5] = ["sbx-mgMwauI", "bvEbhcFpPa8", "pbQ4FAddoHo"]
+f[6] = ["6ZP1DKDLCDU", "yRNk_fEC1Zw", "2v2ksI_sho0" , "AD86CwFYd1c"]
+f[7] = ["fWfbxc1ZxNA", "bvGE6irh_Wc", "675kF6H_aGc", "DxUVHAXKHPM", "SaKlVjIE-TY"]
+f[8] = ["Ojl1ynb7_IU", "NnIgYrfEFnA", "SM0TJHmLZ-E"]
+f[9] = ["AW8axHNAyOI", "CJA0ujk3sRg"]
+f[10] = []
+var l = "https://www.youtube.com/embed/";
 
-f[0] = ["fWfbxc1ZxNA", "bvGE6irh_Wc", "675kF6H_aGc", "DxUVHAXKHPM", "SaKlVjIE-TY"]*/
-
-
-
-$(document).ready(function () {	
-	if(localStorage.getItem("storageName")==0){
-		$(".video").append('<iframe width="700" height="393.92" src="https://www.youtube.com/embed/fWfbxc1ZxNA" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>')
-		$(".other").append('<iframe width="300" height="169.49" src="https://www.youtube.com/embed/bvGE6irh_Wc" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>')
-		$(".other").append('<iframe width="300" height="169.49" src="https://www.youtube.com/embed/675kF6H_aGc" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>')
-		$(".other").append('<iframe width="300" height="169.49" src="https://www.youtube.com/embed/DxUVHAXKHPM" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>')
-		$(".other").append('<iframe width="300" height="169.49" src="https://www.youtube.com/embed/SaKlVjIE-TY" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>')
-		$(".desc").append('<div class="head">Programming with c++</div><div class="long">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </div>')
-	}
-
-	if(localStorage.getItem("storageName")==1){
-		
-	}
-	
-	if(localStorage.getItem("storageName")==2){
-
-	}
-	
-	if(localStorage.getItem("storageName")==3){
-
-	}
-	
-	if(localStorage.getItem("storageName")==4){
-
-	}
-	
-	if(localStorage.getItem("storageName")==5){
-
-	}
-
-	if(localStorage.getItem("storageName")==6){
-
-	}
-
-	if(localStorage.getItem("storageName")==7){
-
-	}
-
-	if(localStorage.getItem("storageName")==8){
-
-	}
-
-	if(localStorage.getItem("storageName")==9){
-
-	}
-
-	if(localStorage.getItem("storageName")==10){
-
-	}
-
-	if(localStorage.getItem("storageName")==11){
-
+$(document).ready(function () {		
+	var len = f[localStorage.getItem("storageName")].length;
+	var tot = l + f[localStorage.getItem("storageName")][0]
+	$(".video").append('<iframe width="700" height="393.92" src="'+tot+'" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>')
+	$(".desc").append('<div class="head">Programming with c++</div><div class="long">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </div>')
+	for(i = 1; i<len;i++){
+		var tot = l + f[localStorage.getItem("storageName")][i];
+		$(".other").append('<iframe width="300" height="169.49" src='+tot+' frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>')
 	}
 })
