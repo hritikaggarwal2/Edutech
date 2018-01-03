dc4eff632e7fb8f2391acea7d6a3e53155659f41
Raghav Mathur <raghav3501@gmail.com>
2018-01-03 11:44:24 +0530

    Vids Complete

--- courses.js
+++ courses.js
@@ -1,77 +1,64 @@
-var inte = "0";
-var engl = "1";
-var net = "2";
-var office = "5";
-var web = "6";
-var cpp = "7";
-var ps = "8";
-var dood = "9";
-var ext = "10";
-
 var abc = document.URL
 var ret = abc.replace('courses.html','vids.html')
 
 $(".inte").click(function () {
-	localStorage.setItem("storageName", inte);
-	console.log(inte);	
-	console.log(localStorage.getItem("storageName"));
+	comm = "0";
+	localStorage.setItem("storageName", comm);
 	window.open(ret, "_self")	
 })
 
 $(".engl").click(function () {
-	localStorage.setItem("storageName", engl);
-	console.log(engl);	
-	console.log(localStorage.getItem("storageName"));
+	comm = "1";
+	localStorage.setItem("storageName", comm);
 	window.open(ret, "_self")	
 })
 
 $(".net").click(function () {
-	localStorage.setItem("storageName", net);
-	console.log(net);	
-	console.log(localStorage.getItem("storageName"));
+	comm = "2";
+	localStorage.setItem("storageName", comm);
+	window.open(ret, "_self")	
+})
+
+$(".entre").click(function () {
+	comm = "3";
+	localStorage.setItem("storageName", comm);
 	window.open(ret, "_self")	
 })
 
 $(".office").click(function () {
-	localStorage.setItem("storageName", office);
-	console.log(office);	
-	console.log(localStorage.getItem("storageName"));
+	comm = "5";
+	localStorage.setItem("storageName", comm);
 	window.open(ret, "_self")	
 })
 
 
 $(".web").click(function () {
-	localStorage.setItem("storageName", web);
-	console.log(web);	
-	console.log(localStorage.getItem("storageName"));
+	comm = "6";
+	localStorage.setItem("storageName", comm);
 	window.open(ret, "_self")	
 })
 
 $(".cpp").click(function () {
-	localStorage.setItem("storageName", cpp);
-	console.log(cpp);	
-	console.log(localStorage.getItem("storageName"));
+	comm = "7";
+	localStorage.setItem("storageName", comm);
 	window.open(ret, "_self")	
 })
 
 $(".ps").click(function () {
-	localStorage.setItem("storageName", ps);
-	console.log(ps);	
-	console.log(localStorage.getItem("storageName"));
+	comm = "8";
+	localStorage.setItem("storageName", comm);
 	window.open(ret, "_self")	
 })
 
 $(".dood").click(function () {
-	localStorage.setItem("storageName", dood);
-	console.log(dood);	
-	console.log(localStorage.getItem("storageName"));
+	comm = "9";
+	localStorage.setItem("storageName", comm);
 	window.open(ret, "_self")	
 })
 
 $(".ext").click(function () {
-	localStorage.setItem("storageName", ext);
-	console.log(ext);	
-	console.log(localStorage.getItem("storageName"));
+	comm = "10";
+	localStorage.setItem("storageName", comm);
 	window.open(ret, "_self")	
 })
 
--- vids.js
+++ vids.js
@@ -11,74 +11,99 @@ f[8] = ["Ojl1ynb7_IU", "NnIgYrfEFnA", "SM0TJHmLZ-E"]
 f[9] = ["AW8axHNAyOI", "CJA0ujk3sRg"]
 f[10] = []
 
+g = ["lorem ipsum", "lorem ipsum", "lorem ipsum", "lorem ipsum", "lorem ipsum", "lorem ipsum", "lorem ipsum", "lorem ipsum", "lorem ipsum",]
+var m = localStorage.getItem("storageName");
 h = ["Using Internet", "English", "Social Network", "Entrepreneurship", "G Suite", "Microsoft Office", "Web Development", "C++ Programming", "Photoshop", "Doodling", "Extensions"]
 var l = "https://www.youtube.com/embed/";
-var ml = f[localStorage.getItem("storageName")];
-$(document).ready(function () {		
-	var len = ml.length;
-	var tot = l + ml[0]
-	$(".desc").append('<div class="head">'+ h[localStorage.getItem("storageName")] +'</div><div class="long">Lorem ipsum dolor sit amet, liquip ex ea commodo	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </div>')
-	$(".convid").append('<iframe class="'+ml[0]+'" src="'+tot+'" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>')
-	for(i = 0; i<len;i++){
-		var tot = l + ml[i];
-		$(".other").append('<div><iframe src='+tot+' frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe><div class="block block'+i+'"></div></div>')
+var ml = f[m];
+$(document).ready(function () {	
+	if (m!=3) {
+		var len = ml.length;
+		var tot = l + ml[0]
+		$(".desc").append('<div class="head">'+ h[m] +'</div><div class="long">'+g[m]+'</div>')
+		$(".convid").append('<iframe class="'+ml[0]+'" src="'+tot+'" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>')
+		for(i = 0; i<len;i++){
+			var tot = l + ml[i];
+			$(".other").append('<div><iframe src='+tot+' frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe><div class="block block'+i+'"></div></div>')
+		}
+
+		$(".block0").click(function () {
+			for(i = 0; i<len;i++){
+				$(".block"+i).css({"background":"transparent"})
+				$("."+ml[i]).remove()
+			}
+			$(".block0").css({"background":"rgba(0, 0, 0, 0.5)"})
+			$(".convid").append('<iframe class="'+ml[0]+'"  src="'+l+ml[0]+'" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>')
+
+		})
+
+		$(".block1").click(function () {
+			for(i = 0; i<len;i++){
+				$(".block"+i).css({"background":"transparent"})
+				$("."+ml[i]).remove()
+			}
+			$(".block1").css({"background":"rgba(0, 0, 0, 0.5)"})
+			$(".convid").append('<iframe class="'+ml[1]+'" src="'+l+ml[1]+'" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>')
+		})
+
+		$(".block2").click(function () {
+			for(i = 0; i<len;i++){
+				$(".block"+i).css({"background":"transparent"})
+				$("."+ml[i]).remove()
+			}
+			$(".block2").css({"background":"rgba(0, 0, 0, 0.5)"})
+			$(".convid").append('<iframe class="'+ml[2]+'"  src="'+l+ml[2]+'" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>')
+		})	
+
+		$(".block3").click(function () {
+			for(i = 0; i<len;i++){
+				$(".block"+i).css({"background":"transparent"})
+				$("."+ml[i]).remove()
+			}
+			$(".block3").css({"background":"rgba(0, 0, 0, 0.5)"})
+			$(".convid").append('<iframe class="'+ml[3]+'" src="'+l+ml[3]+'" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>')
+		})
+
+		$(".block4").click(function () {
+			for(i = 0; i<len;i++){
+				$(".block"+i).css({"background":"transparent"})
+				$("."+ml[i]).remove()
+			}
+			$(".block4").css({"background":"rgba(0, 0, 0, 0.5)"})
+			$(".convid").append('<iframe class="'+ml[4]+'"  src="'+l+ml[4]+'" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>')
+		})
+
+		$(".block5").click(function () {
+			for(i = 0; i<len;i++){
+				$(".block"+i).css({"background":"transparent"})
+				$("."+ml[i]).remove()
+			}
+			$(".block5").css({"background":"rgba(0, 0, 0, 0.5)"})
+			$(".convid").append('<iframe class="'+ml[5]+'"  src="'+l+ml[5]+'" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>')
+		})
+
+	}else{
+		$(".video").css({"width":"100%"})		
+		$(".video").append('<div class="entre"><div>Why Entrepreneurship?</div><div class="star"><a href="">Start Now</a></div></div>')
+		$(".video").append('<div class="entre"><div>Understanding Entrepreneurship</div><div class="star"><a href="">Start Now</a></div></div>')
+		$(".video").append('<div class="entre"><div>Introduction to Customer Discovery</div><div class="star"><a href="">Start Now</a></div></div>')
+		$(".video").append('<div class="entre"><div>Industry, Competition, and Strategy Preparation</div><div class="star"><a href="">Start Now</a></div></div>')
+		$(".video").append('<div class="entre"><div>Project : Domain Expertise</div><div class="star"><a href="">Start Now</a></div></div>')
+		$(".video").append('<div class="entre"><div>Project : Industry Stakeholders and Business Models</div><div class="star"><a href="">Start Now</a></div></div>')
+		$(".video").append('<div class="entre"><div>Project : Project Specs</div><div class="star"><a href="">Start Now</a></div></div>')
+		$(".video").append('<div class="entre"><div>Project : Self Awarness</div><div class="star"><a href="">Start Now</a></div></div>')
+		$(".video").append('<div class="entre"><div>Project : Value Creation Project</div><div class="star"><a href="">Start Now</a></div></div>')
+		$(".video").append('<div class="entre"><div>Resource List</div><div class="star"><a href="">Start Now</a></div></div>')
+		$(".video").append('<div class="entre"><div>Tips On Ted Talks</div><div class="star"><a href="">Start Now</a></div></div>')
+		$(".video").append('<div class="entre"><div>Assignment 1 - Legal and accounting basics for Startups</div><div class="star"><a href="">Start Now</a></div></div>')
+		$(".video").append('<div class="entre"><div>Assignment 2 - Product Design</div><div class="star"><a href="">Start Now</a></div></div>')
+		$(".video").append('<div class="entre"><div>Assignment 3 - Introduction to Intellectual Property</div><div class="star"><a href="">Start Now</a></div></div>')
+		$(".video").append('<div class="entre"><div>Assignment 4 - Finding your Market</div><div class="star"><a href="">Start Now</a></div></div>')
+		$(".video").append('<div class="entre"><div>Assignment 5 - Intro to Venture Capital</div><div class="star"><a href="">Start Now</a></div></div>')
+		$(".video").append('<div class="entre"><div>Assignment 6 - Customer Discovery</div><div class="star"><a href="">Start Now</a></div></div>')
+		$(".convid").css({"display":"none"})		
 	}
-
-	$(".block0").click(function () {
-		for(i = 0; i<len;i++){
-			$(".block"+i).css({"background":"transparent"})
-			$("."+ml[i]).remove()
-		}
-		$(".block0").css({"background":"rgba(0, 0, 0, 0.5)"})
-		$(".convid").append('<iframe class="'+ml[0]+'"  src="'+l+ml[0]+'" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>')
-
-	})
-
-	$(".block1").click(function () {
-		for(i = 0; i<len;i++){
-			$(".block"+i).css({"background":"transparent"})
-			$("."+ml[i]).remove()
-		}
-		$(".block1").css({"background":"rgba(0, 0, 0, 0.5)"})
-		$(".convid").append('<iframe class="'+ml[1]+'" src="'+l+ml[1]+'" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>')
-	})
-
-	$(".block2").click(function () {
-		for(i = 0; i<len;i++){
-			$(".block"+i).css({"background":"transparent"})
-			$("."+ml[i]).remove()
-		}
-		$(".block2").css({"background":"rgba(0, 0, 0, 0.5)"})
-		$(".convid").append('<iframe class="'+ml[2]+'"  src="'+l+ml[2]+'" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>')
-	})	
-
-	$(".block3").click(function () {
-		for(i = 0; i<len;i++){
-			$(".block"+i).css({"background":"transparent"})
-			$("."+ml[i]).remove()
-		}
-		$(".block3").css({"background":"rgba(0, 0, 0, 0.5)"})
-		$(".convid").append('<iframe class="'+ml[3]+'" src="'+l+ml[3]+'" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>')
-	})
-
-	$(".block4").click(function () {
-		for(i = 0; i<len;i++){
-			$(".block"+i).css({"background":"transparent"})
-			$("."+ml[i]).remove()
-		}
-		$(".block4").css({"background":"rgba(0, 0, 0, 0.5)"})
-		$(".convid").append('<iframe class="'+ml[4]+'"  src="'+l+ml[4]+'" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>')
-	})
-
-	$(".block5").click(function () {
-		for(i = 0; i<len;i++){
-			$(".block"+i).css({"background":"transparent"})
-			$("."+ml[i]).remove()
-		}
-		$(".block5").css({"background":"rgba(0, 0, 0, 0.5)"})
-		$(".convid").append('<iframe class="'+ml[5]+'"  src="'+l+ml[5]+'" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>')
-	})
-
+	
 })
 
 
